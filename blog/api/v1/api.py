fd81e19fe38c8c19dcaa30983a24f0659a8e951a
Tom Haines <tomslogic@outlook.com>
2016-11-03 16:46:12 +0000

    Tidied up GitHub API code.
    Added vscode to gitignore

--- blog/api/v1/api.py
+++ blog/api/v1/api.py
@@ -3,8 +3,9 @@ from flask import abort
 from blog import cache, app
 import pickle
 
-class Api():
-    def __init__(self, path, args=""):
+class GitHub():
+    def __init__(self, path, args):
+        self.g = Github(user_agent="TomIsPrettyCool", login_or_token=app.config["GITHUB_ACCESS_TOKEN"])
         self.path = path
         self.route = path.split('/')
 
@@ -20,19 +21,13 @@ class Api():
     def get_result(self):
         return getattr(self, self.route[0])()
 
-    def github(self):
-        # Initialize Github api client then call correc
-        print(self.route[1])
-        g = Github(user_agent="TomIsPrettyCool", login_or_token=app.config["GITHUB_ACCESS_TOKEN"])
-        return getattr(self, "_github_{}".format(self.route[1]))(g)
-
     # Github API methods
-    def _github_get_recent_repos(self, g):
+    def get_recent_repos(self):
 		# Cache and get GitHub repos! Could be done nicer, but it works so isn't a priority
         if cache.exists("github_repos"):
             return pickle.loads(cache.get("github_repos"))
         else:
-            github_user = g.get_user(login="TomIsPrettyCool")
+            github_user = self.g.get_user(login="TomIsPrettyCool")
             repos = github_user.get_repos()
             repo_array = [{"name": x.name, "url": x.html_url} for x in repos]
             cache.set("github_repos", pickle.dumps(repo_array))
--- blog/api/v1/views.py
+++ blog/api/v1/views.py
@@ -1,11 +1,12 @@
 from flask import Blueprint, render_template, redirect, url_for, request, abort, jsonify
-from .api import Api
+from .api import GitHub
 
 api_blueprint = Blueprint('apiv1', __name__)
 
-@api_blueprint.route("/<path:api_path>")
+@api_blueprint.route("/github/<path:api_path>")
 def api_endpoint(api_path):
-    processor = Api(api_path, request.args)
+    print(api_path)
+    processor = GitHub(api_path, request.args)
 
     if processor.valid_api_route():
         return jsonify(processor.get_result())
